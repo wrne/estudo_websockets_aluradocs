eaf3b06283b5efe2ae144abd32a6ccecd1e02681
Wanderley Neto <wneto_ramos@hotmail.com>
2024-02-27 22:28:57 -0300

    Mensagem de texto persistida em memória

--- public/socket-front-documento.js
+++ public/socket-front-documento.js
@@ -3,12 +3,17 @@ import { atualizaTextoEditor } from "./documento.js";
 const socket = io();
 
 function selecionarDocumento(nome){
-	socket.emit("selecionar_documento", nome);
+	socket.emit("selecionar_documento", nome, (texto)=>{atualizaTextoEditor(texto)}); // envio do call para o backend executar a função de atualizar texto economiza um evento de retorno
 }
 
 function emitirTextoEditor(texto, nomeDocumento) {
   socket.emit("texto_editor", texto, nomeDocumento);
 }
+
+// apos envio da AtualizaTextoEditor como callback para o backend, o evento de retorno "documento_encontrado" se torna obsoleto
+// socket.on("documento_encontrado", (texto) => { 
+//   atualizaTextoEditor(texto);
+// });
 
 socket.on("texto_editor_clientes", (texto) => {
   atualizaTextoEditor(texto);
--- src/socket-back.js
+++ src/socket-back.js
@@ -1,15 +1,55 @@
 import io from "./servidor.js";
 
+const documentos = [
+	{
+		nome: "JavaScript",
+		texto: "Texto de Javascript..."
+	},
+	{
+		nome: "Node",
+		texto: "Texto de Node..."
+	},
+	{
+		nome: "Socket.io",
+		texto: "Texto de socket.io..."
+	}
+]
+
 io.on("connection", (socket) => {
-  console.log("Um cliente se conectou! ID:", socket.id);
 
-  socket.on("selecionar_documento",(nomeDocumento)=>{
-	socket.join(nomeDocumento) // "sala de acordo com o documento" - Join cria um agrupamento para transmissao das mensagens
-  })
+	console.log("Um cliente se conectou! ID:", socket.id);
 
-  socket.on("texto_editor", (texto, nomeDocumento) => {
-    // socket.broadcast.emit("texto_editor_clientes", texto); // Dispara para todos os outros sockets
-	socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
-  });
+	socket.on("selecionar_documento", (nomeDocumento, callback) => {
+
+		socket.join(nomeDocumento) // "sala de acordo com o documento" - Join cria um agrupamento para transmissao das mensagens
+
+		const documento = encontrarDocumento(nomeDocumento);
+		console.log(documento);
+		if (documento){
+			// socket.emit("documento_encontrado",documento.texto)  - Forma 1 - Criar um evento de retorno e enviar o texto/documento encontrado
+			callback(documento.texto) // Forma 2 - O Front define e envia uma função para atualziar o texto como callback. No tratamento do evento,o back executa a função definida no front /O\
+
+		}
+
+	})
+
+	socket.on("texto_editor", (texto, nomeDocumento) => {
+		// socket.broadcast.emit("texto_editor_clientes", texto); // Dispara para todos os outros sockets
+
+		const documento = encontrarDocumento(nomeDocumento);
+		if (documento){
+			documento.texto = texto
+			socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
+		}
+	});
 
 });
+
+function encontrarDocumento(nome){
+
+	const documento = documentos.find((doc)=>{
+		return doc.nome === nome;
+	})
+
+	return documento;
+}
